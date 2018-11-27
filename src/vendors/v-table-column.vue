4762fe686827b4244bbaf7dd65344883bbaaaa57
diozhu <diozhu@163.com>
2018-11-27 18:44:14 +0800

    mod

---
+++ src/vendors/v-table-column.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="v-table-column">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'v-table-column',
+        props: {
+            value: {
+                type: Array,
+                default: () => []
+            }
+        },
+        data () {
+            return {
+                currentValue: this.value
+            };
+        },
+        computed: {
+        },
+        mounted () {
+            console.log('v-table-column.mounted: ');
+            this.init();
+        },
+        methods: {
+            init () {
+                console.log('v-table-column.init: ');
+            }
+        }
+    };
+</script>
+<style rel="stylesheet/scss" lang="scss">
+    @import "../scss/variables";
+    @import "../scss/_mixins";
+
+    .v-table-column {
+
+    }
+
+</style>
