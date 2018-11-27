0f149b15934d683ca5e395ea5978a148853e4c36
diozhu <diozhu@163.com>
2018-11-27 16:48:26 +0800

    mod

---
+++ src/vendors/v-table.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="v-table">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'v-table',
+        props: {
+            value: {
+                type: Array,
+                default: () => []
+            }
+        },
+        data () {
+            return {
+                currentValue: this.value
+            };
+        },
+        computed: {
+        },
+        mounted () {
+            console.log('v-table.mounted: ');
+            this.init();
+        },
+        methods: {
+            init () {
+                console.log('v-table.init: ');
+            }
+        }
+    };
+</script>
+<style rel="stylesheet/scss" lang="scss">
+    @import "../scss/variables";
+    @import "../scss/_mixins";
+
+    .v-table{
+
+    }
+
+</style>
